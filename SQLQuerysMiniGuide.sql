150875436f20df283e6c02074c364dac263b99fd
JhordP <j.horda.ny@hotmail.com>
2022-12-28 02:29:53 -0400

    MIN and MAX functions

--- SQLQuerysMiniGuide.sql
+++ SQLQuerysMiniGuide.sql
@@ -27,9 +27,20 @@ SELECT * FROM testTable ORDER BY column1 DESC;
 --Group first by column1 in descendent way, and then by the column3 ascendent when they have the same column1 data.
 SELECT * FROM testTable ORDER BY column1 DESC, column3 ASC;
 
+--Look for Null data
+SELECT * FROM testTable WHERE column2 IS NULL;
+
+--Look for Not Null data
+SELECT * FROM testTable WHERE column2 IS NOT NULL;
+
 --Search a specified quantity of rows in the table
 SELECT TOP 5 column1, column2 FROM testTable WHERE column3 = 'value';
 SELECT TOP 5 PERCENT column1, column2 FROM testTable;
+
+--Search min (smallest) and max (largest) values of the selected column
+SELECT MIN(column1) FROM testTable WHERE column2>=20;
+SELECT MAX(column1) FROM testTable WHERE column2=35;
+
 
 /***************************************************************************************************************************/
 /** INSERTING NEW DATA [INSERT] **/
@@ -41,20 +52,13 @@ INSERT INTO testTable VALUES ('value1', 'value2', 'value3');
 INSERT INTO testTable (column1, column3) VALUES ('value1', 'value2');
 
 /***************************************************************************************************************************/
-/* NULL DATA */
-
---Look for Null data
-SELECT * FROM testTable WHERE column2 IS NULL;
-
---Look for Not Null data
-SELECT * FROM testTable WHERE column2 IS NOT NULL;
-/***************************************************************************************************************************/
-/* UPDATE */
+/* UPDATE EXISTING DATA [UPDATE] */
 
 --Updating data in a row, with a condition. Otherwise, all data including the columns specified will be updated inside the table.
 UPDATE testTable SET column1 = 1, column2 = 'newValue' WHERE column3 = 'value3';
 /***************************************************************************************************************************/
-/* DELETE */
+/* DELETE OR DISCARD DATA [DELETE] */
 
 --Deleting data in a row, with a condition. Otherwise, all data including the columns specified will be deleted inside the table.
 DELETE FROM testTable WHERE column1 = 'value2';
+
